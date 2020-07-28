feedda97c63c07f1ff1ede186b4982970cda83a3
Juno <Laevino@gmail.com>
2020-07-28 13:05:42 -0400

    refactor: Separated typeDefs and resolvers into their separate files in Schemas folder.

--- server/index.js
+++ server/index.js
@@ -1,35 +1,6 @@
-const { ApolloServer, gql } = require('apollo-server');
-// const typeDefs = require('./schemas/typeDefs');
-// const resolvers = require('./schemas/resolvers');
-
-const books = [
-  {
-    title: 'Harry Potter and the Chamber of Secrets',
-    author: 'J.K. Rowling',
-  },
-  {
-    title: 'Jurassic Park',
-    author: 'Michael Crichton',
-  },
-];
-
-const typeDefs = gql`
-type Book {
-  title: String
-  author: String
-}
-
-type Query {
-  books: [Book]
-}
-`;
-
-const resolvers = {
-  Query: {
-    books: () => books,
-  },
-};
-
+const { ApolloServer } = require('apollo-server');
+const typeDefs = require('./schemas/typeDefs');
+const resolvers = require('./schemas/resolvers');
 
 const server = new ApolloServer({
   typeDefs,
